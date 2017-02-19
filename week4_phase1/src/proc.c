9382e6192bacb1c4c8b6b71a4c82327a349b47fc
KatyaNiosi <plimmi.1@gmail.com>
2017-02-19 13:34:12 -0800

    Finished proc

--- week4_phase1/src/proc.c
+++ week4_phase1/src/proc.c
@@ -6,27 +6,34 @@
 #include "spede.h"      // cons_xxx below needs
 #include "data.h"       // current_pid needed below
 #include "proc.h"       // prototypes of processes
-
+//#include "service.h"
+#define LOOP 1666000 // loop to time .6 us
 // Init PID 1, always ready to run, never preempted
 void Init(void) {
    int i;
 
-   forever loop {
-      show on Target PC: "1.." (since Init has PID 1 as we know)
-      loop for LOOP times { // to cause approx 1 second of delay
-         call asm("inb $0x80") which delay .6 microsecond
+   while(1){
+      //show on Target PC: "1.." (since Init has PID 1 as we know)
+	cons_printf("1..");       
+	//loop for LOOP times { // to cause approx 1 second of delay
+	for(i = 0;i<LOOP; i++){
+         //call asm("inb $0x80") which delay .6 microsecond
+	asm("inb $0x80");
       }
    }
 }
 
 // PID 2, 3, 4, etc. mimicking a usual user process
 void UserProc(void) {
-   int i;
+   int i, my_pid;
 
-   forever loop {
-      show on Target PC: "%d..", current_pid (will change to GetPID call later)
-      loop for LOOP times { // to cause approx 1 second of delay
-         call asm("inb $0x80") which delay .6 microsecond
+   while(1){
+      //show on Target PC: "%d..", current_pid (will change to GetPID call later)
+	cons_printf("%d..", current_pid);
+      //loop for LOOP times { // to cause approx 1 second of delay
+	for(i = 0; i < LOOP; i++){
+	//call asm("inb $0x80") which delay .6 microsecond
+	asm("inb $0x80");
       }
    }
 }
