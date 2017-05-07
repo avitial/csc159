40ab0cd935057b609c5b0a05b381bbeb3e4a33f3
leavitia <engineerleam@hotmail.com>
2017-05-06 17:35:11 -0700

    Added files for phase7 (copy from phase6)

---
+++ week12_phase7/src_submitted/handlers.h
@@ -0,0 +1,38 @@
+// handlers.h, 159
+
+#ifndef __HANDLERS_H__
+#define __HANDLERS_H__
+
+#include "types.h"   // need definition of 'func_ptr_t' below
+
+// Time Sharing and Context Switches (phase 1)
+void NewProcHandler(func_ptr_t);
+void TimerHandler(void);
+// Kernel Services(phase 2)
+void SleepHandler(int);
+void GetPidHandler(void);
+// Semaphores IPC (phase 3)
+void SemAllocHandler(int);
+void SemWaitHandler(int);
+void SemPostHandler(int);
+// Device Driver: Busy Poll Printing (phase 4)
+void SysPrintHandler(char *);
+// Device Driver: Interrupt Driven (phase 5)
+void PortWriteOne(int);
+void PortReadOne(int);
+void PortHandler(void);
+void PortAllocHandler(int *);
+void PortWriteHandler(char, int);
+void PortReadHandler(char *, int);
+// File Services (phase 6)
+void FSfindHandler(void);
+void FSopenHandler(void);
+void FSreadHandler(void);
+int FScanAccessFD(int, int);
+int FSallocFD(int);
+dir_t *FSfindName(char *);
+dir_t *FSfindNameSub(char *, dir_t *);
+void FSdir2attr(dir_t *, attr_t *);
+void FScloseHandler(void);
+
+#endif
---
+++ week12_phase7/src_submitted/tools.c
@@ -0,0 +1,92 @@
+// tools.c, 159
+
+#include "spede.h"
+#include "types.h"
+#include "data.h"
+
+// clear DRAM data blocks by filling zeroes
+void MyBzero(char *p, int size) {
+   while(size--) *p++ = 0; // one statement as per Chang's feedback
+}
+
+// dequeue, return 1st integer in array, and move all forward
+// if queue empty, return 0
+int DeQ(q_t *p) { // return 0 if q[] is empty
+  int i, data = 0; 
+	
+  if(p->size == 0){
+    return data;
+  } else{
+    data = p->q[0];
+    p->size--;
+    
+   for(i=0; i<Q_SIZE-1; i++){
+      p->q[i] = p->q[i+1];  
+    }
+  }
+  return data;
+}
+
+// enqueue integer to next available slot in array, size is index
+void EnQ(int data, q_t *p){
+  if(p->size == Q_SIZE){
+    cons_printf("Kernel Panic: queue is full, cannot EnQ!\n");
+    return;
+  }
+  p->q[p->size] = data;
+  p->size++; //= p->size + 1;
+}
+
+//return the length (num of bytes) of a string p points
+int MyStrlen(char *p){
+  int len = 0;
+  while(*p != 0){
+    p++;
+    len++;
+  }
+  return len;
+}
+
+void MyStrcat(char *dst, char *addendum){
+  int str_len, add_len;
+  int j = 0;
+  str_len = MyStrlen(dst);
+  dst += str_len;
+  add_len = MyStrlen(addendum);
+  while(j < add_len){
+    dst[str_len] = addendum[j];
+    str_len++;
+    j++;
+  }
+  dst[add_len] = '\0';
+}
+
+int MyStrcmp(char *p, char *q, int len){ //two ways to do it
+  while(len && p != '\0' && *p == *q){
+  p++;
+  q++;
+  len--;
+  }
+  if(len == 0 || (*p == '\0' && *q =='\0')){
+    return 1;
+  }
+  return 0;
+}
+
+void MyStrcpy(char *dst, char *src){
+  char ch;
+  ch = *src;
+  while(ch != '\0'){
+    *dst = ch;
+    dst++;
+    src++;
+    ch = *src;
+  }
+  *dst = '\0';
+}
+
+void MyMemcpy(char *dst, char *src, int size){
+  while(size--){
+    *dst++=*src++;
+  }
+}
